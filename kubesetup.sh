bb83e51b24a1d44c3539239419ec7d8013d9d701
kubernetes-quickdeploy <99710234+kubernetes-quickdeploy@users.noreply.github.com>
2022-03-28 16:32:30 +0530

    Update kubesetup.sh

--- kubesetup.sh
+++ kubesetup.sh
@@ -3,6 +3,8 @@
 ### Initialize the Kubernetes cluster on the control plane node using kubeadm (Note: This is only performed on the Control Plane Node
 sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.23.0
 
+Sleep 3
+
 ### Set kubectl access ###
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
