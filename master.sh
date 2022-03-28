ca21b2b556a5545beebbd5b51bacf01163b6de87
kubernetes-quickdeploy <99710234+kubernetes-quickdeploy@users.noreply.github.com>
2022-03-28 16:12:03 +0530

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+### Initialize the Kubernetes cluster on the control plane node using kubeadm (Note: This is only performed on the Control Plane Node
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.23.0
+
+### Set kubectl access ###
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+### Install calico ###
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+### Check status of the control plane node ###
+kubectl get nodes
+
