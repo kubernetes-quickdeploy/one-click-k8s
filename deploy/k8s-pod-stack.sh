c133571703e8f60cb68bd2afe2af93acb5f61015
kubernetes-quickdeploy <99710234+kubernetes-quickdeploy@users.noreply.github.com>
2022-03-29 15:53:04 +0530

    Update k8s-pod-stack.sh

--- deploy/k8s-pod-stack.sh
+++ deploy/k8s-pod-stack.sh
@@ -1,1 +1,5 @@
+### Install calico ###
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
+### Kubernrets Dashboard ###
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
