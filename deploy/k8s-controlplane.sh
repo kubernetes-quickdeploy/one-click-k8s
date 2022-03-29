09e2534dded5fbe94b7ef8404e0fd8b28a87e101
kubernetes-quickdeploy <99710234+kubernetes-quickdeploy@users.noreply.github.com>
2022-03-29 15:52:44 +0530

    Update k8s-controlplane.sh

--- deploy/k8s-controlplane.sh
+++ deploy/k8s-controlplane.sh
@@ -10,12 +10,6 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-### Install calico ###
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-
-### Kubernrets Dashboard ###
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
-
 ### Check status of the control plane node ###
 kubectl get nodes
 
