7d3ea8634cb982062ec0636b5fc3103fca814a79
LTPB\U6048317 <suryateja@clarivate.com>
2022-03-29 16:11:32 +0530

    commit

--- deploy/k8s-pod-stack.sh
+++ deploy/k8s-pod-stack.sh
@@ -3,3 +3,4 @@ kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
 ### Kubernrets Dashboard ###
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
+
